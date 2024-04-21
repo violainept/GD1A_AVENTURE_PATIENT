108672146601ae6679da9063650ada0e3f8ebcd3
Violaine <violaine.patient@gmail.com>
2024-04-21 21:09:42 +0200

    Add Personal Sprites

--- GD1A_AVENTURE_PATIENT_02/Assets/Scripts/Managers/UIManager.cs
+++ GD1A_AVENTURE_PATIENT_02/Assets/Scripts/Managers/UIManager.cs
@@ -12,13 +12,11 @@ public class UIManager : MonoBehaviour
     [Header("Bars")]
     [SerializeField] private Image healthBar;
     [SerializeField] private Image manaBar;
-    [SerializeField] private Image expBar;
 
     [Header("Text")]
     [SerializeField] private TextMeshProUGUI levelTMP;
     [SerializeField] private TextMeshProUGUI healthTMP;
     [SerializeField] private TextMeshProUGUI manaTMP;
-    [SerializeField] private TextMeshProUGUI expTMP;
 
     private void Update()
     {
@@ -29,11 +27,9 @@ public class UIManager : MonoBehaviour
     {
         healthBar.fillAmount = Mathf.Lerp(healthBar.fillAmount, stats.Health / stats.MaxHealth, 10f * Time.deltaTime);
         manaBar.fillAmount = Mathf.Lerp(manaBar.fillAmount, stats.Mana / stats.MaxMana, 10f * Time.deltaTime);
-        expBar.fillAmount = Mathf.Lerp(expBar.fillAmount, stats.CurrentExp / stats.NextLevelExp, 10f * Time.deltaTime);
 
         levelTMP.text = $"Level {stats.Level}";
         healthTMP.text = $" {stats.Health}/{stats.MaxHealth}";
         manaTMP.text = $" {stats.Mana}/{stats.MaxMana}";
-        expTMP.text = $"{stats.CurrentExp} / {stats.NextLevelExp}";
     }
 }
